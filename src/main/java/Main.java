262959906345d9c90d22acee55f9601660a24b7d
crimson <151975517+crimsonlmaoo@users.noreply.github.com>
2025-09-06 21:18:02 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ import hmph.rendering.RenderWIndow;
 
 public class Main {
     public static void main(String[] args) {
+        //actual rendering - land is pretty cool for making this ;-)
         RenderWIndow window = new RenderWIndow("Hmph", 1024, 720, true);
+        //init - land again is pretty cool for making this ;-)
         window.init();
     }
 }
